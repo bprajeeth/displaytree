0385832f4ef96c66168618bd1c33ad943637ed3a
bprajeeth <84741032+bprajeeth@users.noreply.github.com>
2022-06-05 01:23:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 VERSION="1.0.0"
 setup(
-  name = 'printree',        
-  packages = ['printree'], 
+  name = 'displaytree',        
+  packages = ['displaytree'], 
   version = VERSION,   
   license='MIT',  
   description = 'Python package that helps you visualize your binary tree',  
   author = 'bprajeeth',               
   author_email = 'bprajeeth285@gmail.com',    
   url = 'https://github.com/bprajeeth/printree',  
-  download_url = 'https://github.com/bprajeeth/printree/archive/refs/tags/v1.0.0.tar.gz',  
+  download_url = 'https://github.com/bprajeeth/displaytree/archive/refs/tags/v1.0.0.tar.gz',  
   keywords = ['BINARY TREE', 'PRINT', 'VISUALIZE', 'TREE', 'PRINT BINARY TREE'], 
   install_requires=[],
   classifiers=[
